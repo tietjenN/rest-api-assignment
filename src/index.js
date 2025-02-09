7e13e6894f1add0825342d2a25c822869b7f2170
Niko <tietjenniko@gmail.com>
2025-02-09 11:08:36 -0500

    Exercise 3 Submission

--- src/index.js
+++ src/index.js
@@ -4,13 +4,91 @@ const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+const uuid = require('uuid')
 
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var users = [];
+
+app.post('/users', (req, res) => {
+    var username = req.body.name;
+    var useremail = req.body.email;
+    if(!username || !useremail){
+        console.log('Missing field');
+        res.status(400, 'Bad Request').end();
+    }
+    else{
+        var newid = uuid.v4();
+        res.status(201).json({id: newid, name: username, email: useremail});
+        users.push({id: newid, name: username, email: useremail});
+        //console.log(users[0]);
+    }
+});
+
+app.get('/users/:id', (req, res) => {
+    var retuser;
+    for(var i = 0; i < users.length; i++){
+        if(users[i].id == req.params.id)
+        {
+            retuser = users[i];
+            break;
+        }
+    }
+    if(!retuser){
+        res.status(404, 'Not Found').end();
+    }
+    else{
+        res.status(200).json(retuser);
+    }
+
+});
+
+app.put('/users/:id', (req, res) => {
+    var username = req.body.name;
+    var useremail = req.body.email;
+    if(!username || !useremail){
+        console.log('Missing field');
+        res.status(400, 'Bad Request').end();
+    }
+    var retuser;
+    for(var i = 0; i < users.length; i++){
+        if(users[i].id == req.params.id)
+        {
+            users[i] = {id: req.params.id, name: username, email: useremail}
+            retuser = users[i];
+            break;
+        }
+    }
+    if(!retuser){
+        res.status(404, 'Not Found').end();
+    }
+    else{
+        res.status(200).json(retuser);
+    }
+
+});
+
+app.delete('/users/:id', (req, res) => {
+    var deleted = false;
+    for(var i = 0; i < users.length; i++){
+        if(users[i].id == req.params.id)
+        {
+            users.splice(i, 1);
+            deleted = true;
+            console.log('Found user');
+            break;
+        }
+    }
+    if(!deleted){
+        res.status(404, 'Not Found').end();
+    }
+    else{
+        console.log('Success');
+        res.status(204, 'No Content').end();
+    }
+
 });
 
 // Do not touch the code below this comment
